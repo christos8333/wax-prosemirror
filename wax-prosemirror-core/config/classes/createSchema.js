c1ffbb4c510c10230aab15ce3edaa286e6eb20b0
chris <kokosias@yahoo.gr>
2019-04-17 16:02:00 +0300

    add file

---
+++ wax-prosemirror-core/config/classes/createSchema.js
@@ -0,0 +1,33 @@
+import { Schema } from "prosemirror-model";
+
+class createSchema {
+  constructor(schema) {
+    if (!schema) {
+      throw new Error("schema is mandatory");
+    }
+    const { nodes, marks } = schema;
+
+    if (!nodes || !marks) {
+      throw new Error("no nodes or marks found");
+    }
+
+    this.nodes = nodes;
+    this.marks = marks;
+    return this.initSchema();
+  }
+
+  initSchema() {
+    return new Schema(this.toJSON());
+  }
+
+  toJSON() {
+    return {
+      nodes: this.nodes,
+      marks: this.marks
+    };
+  }
+
+  setDefaultSchema() {}
+}
+
+export default createSchema;

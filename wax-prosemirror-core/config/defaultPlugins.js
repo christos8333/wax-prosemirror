e0e3961088ee9cae77c3d8b20cf25faadd76e0ab
chris <kokosias@yahoo.gr>
2019-04-17 16:56:14 +0300

    remove unused from core

--- wax-prosemirror-core/config/defaultPlugins.js
+++ wax-prosemirror-core/config/defaultPlugins.js
@@ -2,11 +2,7 @@ import { history } from "prosemirror-history";
 import { dropCursor } from "prosemirror-dropcursor";
 import { gapCursor } from "prosemirror-gapcursor";
 import "prosemirror-gapcursor/style/gapcursor.css";
-
-//TODO remove everything in comments
-// import "prosemirror-tables/style/tables.css";
 import placeholderPlugin from "./plugins/placeholderPlugin";
-// import { columnResizing, tableEditing } from "prosemirror-tables";
 
 // import rules from "./rules";
 
@@ -16,10 +12,4 @@ export default [
   gapCursor(),
   history(),
   placeholderPlugin
-  // columnResizing(),
-  // tableEditing()
 ];
-
-// for tables
-// document.execCommand("enableObjectResizing", false, false);
-// document.execCommand("enableInlineTableEditing", false, false);

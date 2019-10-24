28307f817f5ba53e96917ee3b898a6292a30c825
chris <kokosias@yahoo.gr>
2019-10-24 14:11:06 +0300

    xpub editor

---
+++ editors/xpub/src/Xpub.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Wax, CreateSchema } from "wax-prosemirror-core";
+import { DefaultSchema } from "wax-prosemirror-schema";
+import "wax-prosemirror-layouts/layouts/default-layout.css";
+import "wax-prosemirror-themes/themes/default-theme.css";
+
+const plugins = [];
+const keys = {};
+
+const options = {
+  schema: new CreateSchema(DefaultSchema)
+};
+
+class Xpub extends Component {
+  render() {
+    return (
+      <Wax
+        options={options}
+        autoFocus
+        placeholder="Type Something..."
+        theme="default"
+        layout="default"
+      />
+    );
+  }
+}
+
+export default Xpub;

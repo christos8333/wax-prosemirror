a5cd53daa91e786c4a1a15c884c51a4010f85676
chris <kokosias@yahoo.gr>
2019-11-15 11:56:19 +0200

    create helpers file for editoria schema

--- wax-prosemirror-schema/src/editoria/EditoriaSchema.js
+++ wax-prosemirror-schema/src/editoria/EditoriaSchema.js
@@ -1,3 +1,5 @@
+import { parseFormatList, parseTracks, blockLevelToDOM } from "./helpers";
+
 // Npdes
 const pDOM = ["p", 0],
   brDOM = ["br"],
@@ -7,56 +9,6 @@ const pDOM = ["p", 0],
 const emDOM = ["em", 0],
   strongDOM = ["strong", 0],
   codeDOM = ["code", 0];
-
-const parseFormatList = str => {
-  if (!str) {
-    return [];
-  }
-  let formatList;
-  try {
-    formatList = JSON.parse(str);
-  } catch (error) {
-    return [];
-  }
-  if (!Array.isArray(formatList)) {
-    return [];
-  }
-  return formatList.filter(format => typeof format === "string"); // ensure there are only strings in list
-};
-
-const parseTracks = str => {
-  if (!str) {
-    return [];
-  }
-  let tracks;
-  try {
-    tracks = JSON.parse(str);
-  } catch (error) {
-    return [];
-  }
-  if (!Array.isArray(tracks)) {
-    return [];
-  }
-  return tracks.filter(
-    (
-      track // ensure required fields are present
-    ) =>
-      track.hasOwnProperty("user") &&
-      track.hasOwnProperty("username") &&
-      track.hasOwnProperty("date")
-  );
-};
-
-const blockLevelToDOM = node => {
-  const attrs = node.attrs.track.length
-    ? {
-        class: node.attrs.class,
-        "data-track": JSON.stringify(node.attrs.track)
-      }
-    : { class: node.attrs.class };
-  return attrs;
-};
-
 const EditoriaSchema = {
   nodes: {
     doc: {

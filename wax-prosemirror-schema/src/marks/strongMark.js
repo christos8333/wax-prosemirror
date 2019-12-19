00744342785056d653de17a2f8ebd9076667952a
chris <kokosias@yahoo.gr>
2019-12-19 17:01:00 +0200

    add files

---
+++ wax-prosemirror-schema/src/marks/strongMark.js
@@ -0,0 +1,17 @@
+const strong = {
+  parseDOM: [
+    { tag: "strong" },
+    {
+      tag: "b",
+      getAttrs: node => node.style.fontWeight != "normal" && null
+    },
+    {
+      style: "font-weight",
+      getAttrs: value => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null
+    }
+  ],
+  toDOM(hook, next) {
+    hook.value = ["strong", 0];
+    next();
+  }
+};

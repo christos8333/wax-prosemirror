1b7f69b965f094509c711ed49e8f27148d9f08b4
chris <kokosias@yahoo.gr>
2020-01-17 17:32:34 +0200

    all track change nodes in progress

--- wax-prosemirror-schema/src/nodes/trackChangesNodes/index.js
+++ wax-prosemirror-schema/src/nodes/trackChangesNodes/index.js
@@ -1,5 +1,32 @@
+import paragraphTrackNode from "./paragraphTrackNode";
 import authorTrackNode from "./authorTrackNode";
+import titleTrackNode from "./titleTrackNode";
+import subTitleTrackNode from "./subTitleTrackNode";
+import epigraphProseTrackNode from "./epigraphProseTrackNode";
+import epigraphPoetryTrackNode from "./epigraphPoetryTrackNode";
+import headingTrackNode from "./headingTrackNode";
+import paragraphContTrackNode from "./paragraphContTrackNode";
+import extractProseTrackNode from "./extractProseTrackNode";
+import extractPoetryTrackNode from "./extractPoetryTrackNode";
+import sourceNoteTrackNode from "./sourceNoteTrackNode";
+import bulletListTrackNode from "./bulletListTrackNode";
+import orderedListTrackNode from "./orderedListTrackNode";
+import listItemTrackNode from "./listItemTrackNode";
+import imageTrackNode from "./imageTrackNode";
 
 export default {
+  // paragraph: paragraphTrackNode,
   author: authorTrackNode
+  // title: titleTrackNode,
+  //subtitle: subTitleTrackNode,
+  //epigraphProse: epigraphProseTrackNode,
+  //epigraphPoetry: epigraphPoetryTrackNode,
+  // paragraphCont: paragraphContTrackNode,
+  // extractProse: extractProseTrackNode,
+  // extractPoetry: extractPoetryTrackNode,
+  // sourceNote: sourceNoteTrackNode,
+  // bulletlist: bulletListTrackNode,
+  // orderedlist: orderedListTrackNode,
+  // list_item: listItemTrackNode,
+  // image: imageTrackNode
 };

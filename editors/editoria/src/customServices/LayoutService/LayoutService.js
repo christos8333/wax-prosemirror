6f026934df6fa1a94a20973f34737ed5b6b38b59
chris <kokosias@yahoo.gr>
2020-01-27 20:48:20 +0200

    fix remove custom service

--- editors/editoria/src/customServices/LayoutService/LayoutService.js
+++
@@ -1,8 +0,0 @@
-import { Service } from "wax-prosemirror-core";
-import EditoriaLayout from "./EditoriaLayout";
-export class LayoutService extends Service {
-  boot() {
-    const myLayout = this.container.get("Layout");
-    myLayout.setLayout(EditoriaLayout);
-  }
-}

37ffb717e93e9ad7f7b592a11307476dee199a5e
chris <kokosias@yahoo.gr>
2020-04-09 16:52:30 +0300

    use react transition

--- wax-prosemirror-components/src/components/comments/CommentBox.js
+++ wax-prosemirror-components/src/components/comments/CommentBox.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useContext } from "react";
+import React, { Fragment, useState, useCallback, useEffect } from "react";
+import { Transition } from "react-transition-group";
 import styled from "styled-components";
 
 const CommentBoxStyled = styled.div`
@@ -9,9 +10,35 @@ const CommentBoxStyled = styled.div`
   margin-top: 10px;
   background: black;
   position: absolute;
+  transition: ${({ state }) => "top 1s, opacity 1.5s"};
   top: ${props => (props.top ? `${props.top}px` : 0)};
+
+  opacity: ${({ state }) => {
+    switch (state) {
+      case "exited":
+        return 0.2;
+      case "exiting":
+        return 0.4;
+      case "entering":
+        return 0.6;
+      case "entered":
+        return 1;
+    }
+  }};
 `;
 
 export default ({ mark, view, top }) => {
-  return <CommentBoxStyled top={top} />;
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
+  return (
+    <Fragment>
+      <Transition in={animate} timeout={1000}>
+        {state => <CommentBoxStyled top={top} state={state} />}
+      </Transition>
+    </Fragment>
+  );
 };

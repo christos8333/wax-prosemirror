675fc8c3a6415159c71577c5f769ed2bfe3f4edc
chris <kokosias@yahoo.gr>
2020-05-06 18:03:13 +0300

    transform comment before merging them into document

---
+++ wax-prosemirror-core/src/helpers/TransformPasted.js
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
+import { DocumentHelpers } from "wax-prosemirror-utilities";
+
+const transformPasted = (slice, view) => {
+  const { content } = slice;
+  const commentNodes = DocumentHelpers.findChildrenByMark(
+    content,
+    view.state.schema.marks.comment,
+    true
+  );
+  const allComments = commentNodes.map(node => {
+    return node.node.marks.filter(comment => {
+      return comment.type.name === "comment";
+    });
+  });
+
+  allComments.forEach(comment => {
+    comment[0].attrs.id = uuidv4();
+  });
+  return slice;
+};
+
+export default transformPasted;

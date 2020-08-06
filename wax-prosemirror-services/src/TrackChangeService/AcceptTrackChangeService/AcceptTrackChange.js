46d002944e3ee03dc5e4ad063678d20f51c1eebb
chris <kokosias@yahoo.gr>
2020-08-06 18:29:16 +0300

    accept all insertions in selection

--- wax-prosemirror-services/src/TrackChangeService/AcceptTrackChangeService/AcceptTrackChange.js
+++ wax-prosemirror-services/src/TrackChangeService/AcceptTrackChangeService/AcceptTrackChange.js
@@ -8,7 +8,29 @@ class AcceptTrackChange extends Tools {
   content = 'Accept';
 
   get run() {
-    return (state, dispatch) => {};
+    return (state, dispatch) => {
+      const {
+        selection: { from, to },
+      } = state;
+
+      state.doc.nodesBetween(from, to, (node, pos) => {
+        if (
+          node.marks &&
+          node.marks.find(mark => mark.type.name === 'insertion')
+        ) {
+          const insertionMark = node.marks.find(
+            mark => mark.type.name === 'insertion',
+          );
+          dispatch(
+            state.tr.removeMark(
+              insertionMark.pos,
+              insertionMark.pos + node.nodeSize,
+              state.schema.marks.insertion,
+            ),
+          );
+        }
+      });
+    };
   }
 
   get active() {

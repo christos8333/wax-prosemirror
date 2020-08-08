c6faf222fc153910ea8b47ead3682f8032f3396e
chris <kokosias@yahoo.gr>
2020-08-08 12:47:26 +0300

    add file

---
+++ wax-prosemirror-services/src/TrackChangeService/track-changes/helpers/removeNode.js
@@ -0,0 +1,23 @@
+import {
+  ReplaceStep,
+  ReplaceAroundStep,
+  replaceStep,
+} from 'prosemirror-transform';
+import { Slice } from 'prosemirror-model';
+import { Selection, TextSelection, EditorState } from 'prosemirror-state';
+import { liftListItem } from 'prosemirror-schema-list';
+
+const removeNode = (tr, node, nodePos, map, accept) => {
+  const newNodePos = map.map(nodePos);
+  const selectionBefore = Selection.findFrom(tr.doc.resolve(newNodePos), -1);
+  const start = selectionBefore.$anchor.pos;
+  const end = newNodePos + 1;
+
+  const delStep = ReplaceStep(tr.doc, start, end);
+
+  tr.step(delStep);
+  const stepMap = delStep.getMap();
+  map.appendMap(stepMap);
+};
+
+export default removeNode;

d5c6c8e71e92785c970bf6a62687a2de43c8ca33
chris <kokosias@yahoo.gr>
2020-09-25 21:01:16 +0300

    loop through each child

--- wax-prosemirror-schema/src/nodes/footNoteNode.js
+++ wax-prosemirror-schema/src/nodes/footNoteNode.js
@@ -4,16 +4,19 @@ const getHTMLString = node => {
 
   if (textContent) {
     let strContent = textContent;
-    for (let i = 0; i < node.content.content[0].marks.length; i += 1) {
-      const mark = node.content.content[0].marks[i];
-      const { type: markType } = mark;
-      const domDetails = markType.spec.toDOM(mark);
-      const htmlAttrs = Object.keys(domDetails[1]).reduce(
-        (str, key) => `${str}${key}="${domDetails[1][key]}"`,
-        '',
-      );
-      const htmlTag = domDetails[0];
-      strContent = `<${htmlTag} ${htmlAttrs}>${strContent}</${htmlTag}>`;
+    for (let i = 0; i < node.content.content.length; i += 1) {
+      for (let j = 0; j < node.content.content[i].marks.length; j += 1) {
+        const mark = node.content.content[i].marks[j];
+
+        const { type: markType } = mark;
+        const domDetails = markType.spec.toDOM(mark);
+        const htmlAttrs = Object.keys(domDetails[1]).reduce(
+          (str, key) => `${str}${key}="${domDetails[1][key]}"`,
+          '',
+        );
+        const htmlTag = domDetails[0];
+        strContent = `<${htmlTag} ${htmlAttrs}>${strContent}</${htmlTag}>`;
+      }
     }
     return strContent;
   }

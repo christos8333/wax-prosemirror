e9b018eb9d02873a1ad36b95a7fdde3468e7ff86
chris <kokosias@yahoo.gr>
2020-09-25 20:34:54 +0300

    get HTML string of footnote

--- wax-prosemirror-schema/src/nodes/footNoteNode.js
+++ wax-prosemirror-schema/src/nodes/footNoteNode.js
@@ -1,4 +1,24 @@
 // TODO Write the node in WaxSchema
+const getHTMLString = node => {
+  const { textContent } = node;
+
+  if (textContent) {
+    let strContent = textContent;
+    for (let i = 0; i < node.content.content[0].marks.length; i += 1) {
+      const mark = node.content.content[0].marks[i];
+      const { type: markType } = mark;
+      const domDetails = markType.spec.toDOM(mark);
+      const htmlAttrs = Object.keys(domDetails[1]).reduce(
+        (str, key) => `${str}${key}="${domDetails[1][key]}"`,
+        '',
+      );
+      const htmlTag = domDetails[0];
+      strContent = `<${htmlTag} ${htmlAttrs}>${strContent}</${htmlTag}>`;
+    }
+    return strContent;
+  }
+};
+
 const footnote = {
   group: 'inline',
   content: 'inline*',
@@ -8,6 +28,7 @@ const footnote = {
     id: { default: '' },
   },
   toDOM: node => {
+    console.log(getHTMLString(node));
     return ['footnote', node.attrs];
   },
   parseDOM: [

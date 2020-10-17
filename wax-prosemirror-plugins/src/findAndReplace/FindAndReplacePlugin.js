963f3ffd8b00453e65b0cb8369a1e7a310d845e2
chris <kokosias@yahoo.gr>
2020-10-17 18:42:20 +0300

    plugin file

---
+++ wax-prosemirror-plugins/src/findAndReplace/FindAndReplacePlugin.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import { Plugin, PluginKey } from 'prosemirror-state';
+import { Decoration, DecorationSet } from 'prosemirror-view';
+
+const findAndReplacePlugin = new PluginKey('findAndReplacePlugin');
+
+export default props => {
+  return new Plugin({
+    key: findAndReplacePlugin,
+    state: {
+      init: (_, state) => {
+        return {};
+      },
+      apply(tr, prev, _, newState) {},
+    },
+    props: {
+      decorations: state => {
+        const findAndReplacePluginState =
+          state && findAndReplacePlugin.getState(state);
+        // return findAndReplacePluginState.createDecoration;
+      },
+      getSearchTerm: state => {},
+    },
+  });
+};
--- wax-prosemirror-services/src/FindAndReplaceService/FindAndReplaceService.js
+++ wax-prosemirror-services/src/FindAndReplaceService/FindAndReplaceService.js
@@ -1,10 +1,16 @@
+import { FindAndReplacePlugin } from 'wax-prosemirror-plugins';
 import Service from '../Service';
 import FindAndReplace from './FindAndReplace';
 
 class FindAndReplaceService extends Service {
   name = 'FindAndReplaceService';
 
-  boot() {}
+  boot() {
+    this.app.PmPlugins.add(
+      'findAndReplacePlugin',
+      FindAndReplacePlugin('findAndReplacePlugin'),
+    );
+  }
 
   register() {
     this.container.bind('FindAndReplace').to(FindAndReplace);

5392ad26b945640f9ba0ec492a9cea46fba393e3
chris <kokosias@yahoo.gr>
2021-01-01 06:36:50 +0200

    counter styles

--- wax-prosemirror-components/src/icons/icons.js
+++ wax-prosemirror-components/src/icons/icons.js
@@ -200,6 +200,12 @@ export default {
       <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z" />
     </Svg>
   ),
+  arrowDown: ({ className }) => (
+    <Svg className={className} viewBox="0 0 24 24">
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
+    </Svg>
+  ),
   codeBlock: ({ className }) => (
     <Svg
       className={className}

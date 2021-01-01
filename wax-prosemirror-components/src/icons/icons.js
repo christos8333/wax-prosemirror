357f8e47150c0c1b14b9906ec927be48e74391c5
chris <kokosias@yahoo.gr>
2021-01-01 10:49:26 +0200

    add new icons

--- wax-prosemirror-components/src/icons/icons.js
+++ wax-prosemirror-components/src/icons/icons.js
@@ -274,9 +274,29 @@ export default {
     </Svg>
   ),
   matchCase: ({ className }) => (
-    <Svg className={className} viewBox="0 0 24 24" fill="none">
+    <Svg className={className} fill="none" viewBox="0 0 24 24">
       <title> Match Case </title>
       <path d="M2.5,4v3h5v12h3V7h5V4H2.5z M21.5,9h-9v3h3v7h3v-7h3V9z" />
+    </Svg>
+  ),
+  editing: ({ className }) => (
+    <Svg className={className} fill="none" viewBox="0 0 24 24">
+      <title> Editing </title>
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+    </Svg>
+  ),
+  suggesting: ({ className }) => (
+    <Svg className={className} fill="none" viewBox="0 0 24 24">
+      <title> Suggesting </title>
+      <path d="M22,24H2v-4h20V24z M13.06,5.19l3.75,3.75L7.75,18H4v-3.75L13.06,5.19z M17.88,7.87l-3.75-3.75 l1.83-1.83c0.39-0.39,1.02-0.39,1.41,0l2.34,2.34c0.39,0.39,0.39,1.02,0,1.41L17.88,7.87z" />
+    </Svg>
+  ),
+  viewing: ({ className }) => (
+    <Svg className={className} fill="none" viewBox="0 0 24 24">
+      <title> Viewing </title>
+      <path d="M0 0h24v24H0z" fill="none" />{' '}
+      <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />{' '}
     </Svg>
   ),
   specialCharacters: ({ className }) => (

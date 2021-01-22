b6a9e7b6e32455efe225f357dd74b50d15b824df
chris <kokosias@yahoo.gr>
2021-01-22 03:57:56 +0200

    marks for editoria

--- wax-prosemirror-schema/src/marks/emphasisMark.js
+++ wax-prosemirror-schema/src/marks/emphasisMark.js
@@ -1,9 +1,9 @@
 const em = {
-  parseDOM: [{ tag: "i" }, { tag: "em" }, { style: "font-style=italic" }],
+  parseDOM: [{ tag: 'i' }, { tag: 'em' }, { style: 'font-style=italic' }],
   toDOM(hook, next) {
-    hook.value = ["em", 0];
+    hook.value = ['em', 0];
     next();
-  }
+  },
 };
 
 export default em;

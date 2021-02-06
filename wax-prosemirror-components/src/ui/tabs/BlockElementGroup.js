8c96d0651660078ea6db90b928ad8431190f214a
chris <kokosias@yahoo.gr>
2021-02-07 01:22:58 +0200

    linter fixes

--- wax-prosemirror-components/src/ui/tabs/BlockElementGroup.js
+++ wax-prosemirror-components/src/ui/tabs/BlockElementGroup.js
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
+import { th } from '@pubsweet/ui-toolkit';
 import styled from 'styled-components';
 import CustomTagBlockComponent from '../../components/customtag/CustomTagBlockComponent';
 import BlockElement from './BlockElement';
-import { th } from '@pubsweet/ui-toolkit';
 
 const Wrapper = styled.div``;
 
 const GroupName = styled.div`
-  margin-bottom: 4px;
   font-size: 14px;
+  margin-bottom: 4px;
   text-transform: uppercase;
 `;
 
@@ -32,64 +33,72 @@ const IconPlusSVG = props => {
   );
 };
 
-
 const Icon = styled(IconPlusSVG)`
+  cursor: pointer;
+  fill: ${th('colorPrimary')};
   height: 10px;
   width: 10px;
-  fill: ${th('colorPrimary')};
-  cursor: pointer;
 `;
 
 const IconCrossSVG = props => {
   const { className } = props;
   return (
-    <svg className={className} viewBox="0 0 409.6 409.6" fill="none" >
-      <path d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467
-  c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z"/>
+    <svg className={className} viewBox="0 0 409.6 409.6" fill="none">
+      <path
+        d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467
+  c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z"
+      />
     </svg>
   );
 };
 
 const IconCross = styled(IconCrossSVG)`
+  cursor: pointer;
+  fill: ${th('colorPrimary')};
   height: 10px;
   width: 10px;
-  fill: ${th('colorPrimary')};
-  cursor: pointer;
 `;
-
 
 const BlockElementGroup = props => {
   const { groupName, items, view } = props;
   const [isIconClicked, setIconClicked] = useState(false);
 
   const onIconClick = () => {
-    setIconClicked(isIconClicked === true ? false : true);
-  }
+    setIconClicked(!isIconClicked);
+  };
 
   return (
     <Wrapper>
-      {groupName !== "Custom Block" && <>
-        <GroupName>{groupName}</GroupName>
-        <ListWrapper>
-          {items &&
-            items.map(item => (
-              <BlockElement key={item.name} item={item} view={view} />
-            ))}
-        </ListWrapper></>}
-
-      {groupName === "Custom Block" && <>
-        <FlexDiv>
+      {groupName !== 'Custom Block' && (
+        <>
           <GroupName>{groupName}</GroupName>
-          <div onClick={onIconClick}>
-            {isIconClicked === false && <Icon />}
-            {isIconClicked === true && <IconCross />}
-          </div>
-        </FlexDiv>
-        <ListWrapper>
-          <CustomTagBlockComponent isShowTag={isIconClicked} view={view} item={items[0]}  />
-        </ListWrapper>
-      </>}
+          <ListWrapper>
+            {items &&
+              items.map(item => (
+                <BlockElement item={item} key={item.name} view={view} />
+              ))}
+          </ListWrapper>
+        </>
+      )}
 
+      {groupName === 'Custom Block' && (
+        <>
+          <FlexDiv>
+            <GroupName>{groupName}</GroupName>
+            <div onClick={onIconClick}>
+              {isIconClicked === false && <Icon />}
+              {isIconClicked === true && <IconCross />}
+            </div>
+          </FlexDiv>
+          <ListWrapper>
+            <CustomTagBlockComponent
+              isShowTag={isIconClicked}
+              item={items[0]}
+              view={view}
+            />
+          </ListWrapper>
+        </>
+      )}
     </Wrapper>
   );
 };

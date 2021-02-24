496b85dfae6afcf11c3bc0f20a8cdc48d430c740
chris <kokosias@yahoo.gr>
2021-02-24 21:29:17 +0200

    move find matches to utilities

--- wax-prosemirror-components/src/components/findAndReplace/helpers.js
+++ wax-prosemirror-components/src/components/findAndReplace/helpers.js
@@ -1,67 +1,15 @@
-import { each, eachRight } from 'lodash';
+import { each } from 'lodash';
 import { DocumentHelpers } from 'wax-prosemirror-utilities';
 import { TextSelection } from 'prosemirror-state';
-
-const findMatches = (doc, searchValue, matchCase) => {
-  const allNodes = [];
-
-  doc.descendants((node, pos) => {
-    allNodes.push({ node, pos });
-  });
-
-  eachRight(allNodes, (node, index) => {
-    if (node.node.type.name === 'footnote') {
-      allNodes.splice(index + 1, node.node.childCount);
-    }
-  });
-
-  const results = [];
-  const mergedTextNodes = [];
-  let index = 0;
-
-  allNodes.forEach((node, i) => {
-    if (node.node.isText) {
-      if (mergedTextNodes[index]) {
-        mergedTextNodes[index] = {
-          text: mergedTextNodes[index].text + node.node.text,
-          pos: mergedTextNodes[index].pos,
-        };
-      } else {
-        mergedTextNodes[index] = {
-          text: node.node.text,
-          pos: node.pos,
-        };
-      }
-    } else {
-      index += 1;
-    }
-  });
-  mergedTextNodes.forEach(({ text, pos }) => {
-    const search = RegExp(escapeRegExp(searchValue), matchCase ? 'gu' : 'gui');
-    let m;
-    // eslint-disable-next-line no-cond-assign
-    while ((m = search.exec(text))) {
-      if (m[0] === '') {
-        break;
-      }
-
-      results.push({
-        from: pos + m.index,
-        to: pos + m.index + m[0].length,
-      });
-    }
-  });
-  return results;
-};
-
-const escapeRegExp = string => {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-};
 
 const getMatchesByView = (views, searchValue, matchCase) => {
   let allResults = 0;
   each(views, (singleView, viewId) => {
-    const results = findMatches(singleView.state.doc, searchValue, matchCase);
+    const results = DocumentHelpers.findMatches(
+      singleView.state.doc,
+      searchValue,
+      matchCase,
+    );
     allResults += results.length;
   });
   return allResults;
@@ -72,7 +20,7 @@ const getAllResultsByView = (view, searchValue, matchCaseSearch) => {
 
   each(view, (singleView, viewId) => {
     if (!allResults[viewId]) {
-      allResults[viewId] = findMatches(
+      allResults[viewId] = DocumentHelpers.findMatches(
         singleView.state.doc,
         searchValue,
         matchCaseSearch,
@@ -214,7 +162,6 @@ const findViewWithMatchesBackWards = (results, view, lastActiveViewId) => {
 };
 
 export default {
-  findMatches,
   getMatchesByView,
   getAllResultsByView,
   getNotesIds,

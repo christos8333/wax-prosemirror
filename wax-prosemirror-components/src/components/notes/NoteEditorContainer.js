bcb32ef0f53590a65c8067e587772a04a4e9eb13
chris <kokosias@yahoo.gr>
2021-04-14 19:24:03 +0300

    style fixes

--- wax-prosemirror-components/src/components/notes/NoteEditorContainer.js
+++ wax-prosemirror-components/src/components/notes/NoteEditorContainer.js
@@ -7,12 +7,16 @@ import NoteNumber from './NoteNumber';
 const NoteEditorContainerStyled = styled.div`
   display: flex;
   flex-direction: row;
-  padding-left: ${grid(10)};
-  padding-right: ${grid(10)};
+  padding-left: ${grid(6)};
   position: relative;
   margin-bottom: 5px;
   width: 100%;
-  box-shadow: 0 0 8px #ecedf1;
+
+  .ProseMirror {
+    padding-right: ${grid(10)};
+    padding-left: ${grid(2)};
+    box-shadow: 0 0 8px #ecedf1;
+  }
 `;
 
 const NoteStyled = styled.div`

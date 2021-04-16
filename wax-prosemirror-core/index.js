52bbdcd5f5d68f80b54a03c090f9e72d17d2fbef
chris <kokosias@yahoo.gr>
2021-04-16 13:45:00 +0300

    cleanup files

--- wax-prosemirror-core/index.js
+++ wax-prosemirror-core/index.js
@@ -2,4 +2,3 @@ export { WaxContext, useInjection } from './src/WaxContext';
 export { PortalContext } from './src/PortalContext';
 export { default as ComponentPlugin } from './src/ComponentPlugin';
 export { default as Wax } from './src/Wax';
-export { default as useReactNodeView } from './src/ReactNodeView';

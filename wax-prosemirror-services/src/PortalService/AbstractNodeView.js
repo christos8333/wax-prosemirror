d4da35d9268eed1c11f3e4aed2d579f4dc6926bc
chris <kokosias@yahoo.gr>
2021-04-26 10:15:06 +0300

    abstract node view

--- wax-prosemirror-services/src/PortalService/AbstractNodeView.js
+++ wax-prosemirror-services/src/PortalService/AbstractNodeView.js
@@ -3,14 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export default class AbstractNodeView {
   constructor(node, view, getPos, decorations, createPortal, Component) {
     this.dom = document.createElement('div');
-    // this.dom.setAttribute('contenteditable', true);
-<<<<<<< HEAD
-    const testDiv = document.createElement('div');
 
-    testDiv.setAttribute('id', 'test');
-    this.dom.append(testDiv);
-=======
->>>>>>> fix
     this.dom.id = uuidv4();
     this.dom.classList.add('portal');
 

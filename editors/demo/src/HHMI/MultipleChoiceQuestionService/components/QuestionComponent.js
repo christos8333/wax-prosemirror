4b5d650861952eacdec8d42e986163533eef12ae
chris <kokosias@yahoo.gr>
2021-04-28 13:21:40 +0300

    move editor to seperate file

--- editors/demo/src/HHMI/MultipleChoiceQuestionService/components/QuestionComponent.js
+++ editors/demo/src/HHMI/MultipleChoiceQuestionService/components/QuestionComponent.js
@@ -1,13 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
-import { EditorView } from 'prosemirror-view';
-import { EditorState } from 'prosemirror-state';
-import { StepMap } from 'prosemirror-transform';
-import { keymap } from 'prosemirror-keymap';
-import { baseKeymap } from 'prosemirror-commands';
-import { undo, redo } from 'prosemirror-history';
-import { WaxContext } from 'wax-prosemirror-core';
+
+import EditorComponent from './EditorComponent';
 
 const QuestionWrapper = styled.div`
   border: 1px solid black;
@@ -35,94 +30,21 @@ const Question = styled.div`
 export default ({ node, view, getPos }) => {
   const [showExplanation, setShowExplanation] = useState(false);
   const [explanationValue, setExplanationValue] = useState('');
-  const editorRef = useRef();
   const explanationRef = useRef(null);
-
-  const context = useContext(WaxContext);
-  let questionView;
-  const questionId = node.attrs.id;
-
-  useEffect(() => {
-    questionView = new EditorView(
-      { mount: editorRef.current },
-      {
-        state: EditorState.create({
-          doc: node,
-          plugins: [keymap(createKeyBindings()), ...context.app.getPlugins()],
-        }),
-        // This is the magic part
-        dispatchTransaction,
-        handleDOMEvents: {
-          mousedown: () => {
-            context.updateView({}, questionId);
-            // Kludge to prevent issues due to the fact that the whole
-            // footnote is node-selected (and thus DOM-selected) when
-            // the parent editor is focused.
-            if (questionView.hasFocus()) questionView.focus();
-          },
-        },
-
-        attributes: {
-          spellcheck: 'false',
-        },
-      },
-    );
-
-    //Set Each note into Wax's Context
-    context.updateView(
-      {
-        [questionId]: questionView,
-      },
-      questionId,
-    );
-  }, []);
 
   const onChangeExplanationInput = () => {
     setExplanationValue(explanationRef.current.value);
   };
 
-  const dispatchTransaction = tr => {
-    let { state, transactions } = questionView.state.applyTransaction(tr);
-    questionView.updateState(state);
-    context.updateView({}, questionId);
-
-    if (!tr.getMeta('fromOutside')) {
-      let outerTr = view.state.tr,
-        offsetMap = StepMap.offset(getPos() + 1);
-      for (let i = 0; i < transactions.length; i++) {
-        let steps = transactions[i].steps;
-        for (let j = 0; j < steps.length; j++)
-          outerTr.step(steps[j].map(offsetMap));
-      }
-      if (outerTr.docChanged) view.dispatch(outerTr);
-    }
-  };
-
   const clickMe = () => {
     setShowExplanation(!showExplanation);
-    // view.dispatch(view.state.tr);
-  };
-
-  const createKeyBindings = () => {
-    const keys = getKeys();
-    Object.keys(baseKeymap).forEach(key => {
-      keys[key] = baseKeymap[key];
-    });
-    return keys;
-  };
-
-  const getKeys = () => {
-    return {
-      'Mod-z': () => undo(view.state, view.dispatch),
-      'Mod-y': () => redo(view.state, view.dispatch),
-    };
   };
 
   return (
     <QuestionWrapper>
       <Question>
         <input type="checkbox" />
-        <div ref={editorRef} />
+        <EditorComponent node={node} view={view} getPos={getPos} />
       </Question>
       <button onClick={clickMe}>Show Explanation</button>
       {showExplanation && (

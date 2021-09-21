81e9893b177223ea08d1c170eea752c621eeb04f
chris <kokosias@yahoo.gr>
2021-09-21 12:44:08 +0300

    temp fix for injecting schema

--- wax-prosemirror-services/src/InlineAnnotations/UnderlineService/UnderlineService.js
+++ wax-prosemirror-services/src/InlineAnnotations/UnderlineService/UnderlineService.js
@@ -1,22 +1,22 @@
-import Service from "../../Service";
-import { toggleMark } from "prosemirror-commands";
-import { underlineMark } from "wax-prosemirror-schema";
-import Underline from "./Underline";
+import { toggleMark } from 'prosemirror-commands';
+import { underlineMark } from 'wax-prosemirror-schema';
+import Underline from './Underline';
+import Service from '../../Service';
 
 class UnderlineService extends Service {
   boot() {
-    const shortCuts = this.container.get("ShortCuts");
-    shortCuts.addShortCut({ "Mod-u": toggleMark(this.schema.marks.underline) });
+    const shortCuts = this.container.get('ShortCuts');
+    shortCuts.addShortCut({ 'Mod-u': toggleMark(this.schema.marks.underline) });
   }
 
   register() {
-    this.container.bind("Underline").to(Underline);
-    const createMark = this.container.get("CreateMark");
+    this.container.bind('Underline').to(Underline);
+    const createMark = this.container.get('CreateMark');
     createMark(
       {
-        underline: underlineMark
+        underline: underlineMark,
       },
-      { toWaxSchema: true }
+      { toWaxSchema: true },
     );
   }
 }

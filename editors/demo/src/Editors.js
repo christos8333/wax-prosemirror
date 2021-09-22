80d148fdb9b9066a1bc687b12d703b19bec9a9a9
chris <kokosias@yahoo.gr>
2021-09-22 20:56:02 +0300

    change title

--- editors/demo/src/Editors.js
+++ editors/demo/src/Editors.js
@@ -96,7 +96,7 @@ const Editors = () => {
             isActive={project === 'ncbi'}
             onClick={() => setProject('ncbi')}
           >
-            Mini Editors
+            Basic Editors
           </ProjectButton>
         </Projects>
       </ChooseProject>

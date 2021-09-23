b91e323425944e3068a51e5d2da5eef811be9865
chris <kokosias@yahoo.gr>
2021-09-23 16:35:52 +0300

    build index.css

--- wax-prosemirror-services/rollup.config.js
+++ wax-prosemirror-services/rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-import-css';
 
 export default {
   input: 'index.js',
@@ -13,6 +14,7 @@ export default {
     },
   ],
   plugins: [
+    css(),
     external({
       includeDependencies: true,
     }),

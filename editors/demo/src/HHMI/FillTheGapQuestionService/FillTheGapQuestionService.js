0b241bf0c68142aec32139503eb580e74feedd1d
chris <kokosias@yahoo.gr>
2021-09-26 06:13:09 +0300

    add new tool

--- editors/demo/src/HHMI/FillTheGapQuestionService/FillTheGapQuestionService.js
+++ editors/demo/src/HHMI/FillTheGapQuestionService/FillTheGapQuestionService.js
@@ -1,7 +1,9 @@
 import { Service } from 'wax-prosemirror-services';
+import FillTheGapQuestion from './FillTheGapQuestion';
 
 class FillTheGapQuestionService extends Service {
   register() {
+    this.container.bind('FillTheGapQuestion').to(FillTheGapQuestion);
     const createNode = this.container.get('CreateNode');
     const addPortal = this.container.get('AddPortal');
   }

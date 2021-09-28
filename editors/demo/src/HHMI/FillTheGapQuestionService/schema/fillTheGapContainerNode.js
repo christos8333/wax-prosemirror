0fe9080382e59a0f419258f95e96deb3ca14d20d
chris <kokosias@yahoo.gr>
2021-09-28 17:20:11 +0300

    new nodes

---
+++ editors/demo/src/HHMI/FillTheGapQuestionService/schema/fillTheGapContainerNode.js
@@ -0,0 +1,26 @@
+const fillTheGapContainerNode = {
+  attrs: {
+    id: { default: '' },
+    class: { default: 'fill-the-gap' },
+  },
+  group: 'block',
+  selectable: true,
+  draggable: true,
+  content: 'block+',
+  parseDOM: [
+    {
+      tag: 'div.fill-the-gap',
+      getAttrs(dom) {
+        return {
+          id: dom.dataset.id,
+          class: dom.getAttribute('class'),
+        };
+      },
+    },
+  ],
+  toDOM(node) {
+    return ['div', node.attrs, 0];
+  },
+};
+
+export default fillTheGapContainerNode;

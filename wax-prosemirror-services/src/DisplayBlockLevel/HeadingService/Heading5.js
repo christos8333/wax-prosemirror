f46e59dcc4a872715c9ed33019683e57dcfc25d1
chris <kokosias@yahoo.gr>
2021-10-15 22:36:45 +0300

    add extra headers

---
+++ wax-prosemirror-services/src/DisplayBlockLevel/HeadingService/Heading5.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { injectable } from 'inversify';
+import { isEmpty } from 'lodash';
+import { LeftSideButton } from 'wax-prosemirror-components';
+import { Commands } from 'wax-prosemirror-utilities';
+import Tools from '../../lib/Tools';
+
+export default
+@injectable()
+class Heading2 extends Tools {
+  title = 'Change to heading level 5';
+  label = 'Heading 5';
+  name = 'Heading5';
+
+  get run() {
+    return (state, dispatch) => {
+      Commands.setBlockType(state.config.schema.nodes.heading5)(
+        state,
+        dispatch,
+      );
+    };
+  }
+
+  get active() {
+    return (state, activeViewId) => {
+      let isActive = false;
+      if (activeViewId !== 'main') return false;
+
+      const { from, to } = state.selection;
+      state.doc.nodesBetween(from, to, (node, pos) => {
+        if (node.type.name === 'heading5') {
+          isActive = true;
+        }
+      });
+      return isActive;
+    };
+  }
+
+  select = (state, activeViewId) => {
+    if (activeViewId !== 'main') return false;
+    return true;
+  };
+
+  get enable() {
+    return state => {
+      return Commands.setBlockType(state.config.schema.nodes.heading5)(state);
+    };
+  }
+
+  renderTool(view) {
+    if (isEmpty(view)) return null;
+    // eslint-disable-next-line no-underscore-dangle
+    return this._isDisplayed ? (
+      <LeftSideButton item={this.toJSON()} key="Heading5" view={view} />
+    ) : null;
+  }
+}
--- wax-prosemirror-services/src/DisplayBlockLevel/HeadingService/HeadingService.js
+++ wax-prosemirror-services/src/DisplayBlockLevel/HeadingService/HeadingService.js
@@ -1,15 +1,17 @@
 import Heading2 from './Heading2';
 import Heading3 from './Heading3';
 import Heading4 from './Heading4';
+import Heading5 from './Heading5';
+import Heading6 from './Heading6';
 import Service from '../../Service';
 
 class HeadingService extends Service {
-  // boot() {}
-
   register() {
     this.container.bind('Heading2').to(Heading2);
     this.container.bind('Heading3').to(Heading3);
     this.container.bind('Heading4').to(Heading4);
+    this.container.bind('Heading5').to(Heading5);
+    this.container.bind('Heading6').to(Heading6);
     const createNode = this.container.get('CreateNode');
     createNode({
       heading2: {
@@ -53,6 +55,36 @@ class HeadingService extends Service {
         ],
         toDOM(node) {
           return ['h4', 0];
+        },
+      },
+    });
+    createNode({
+      heading5: {
+        content: 'inline*',
+        group: 'block',
+        defining: true,
+        parseDOM: [
+          {
+            tag: 'h5',
+          },
+        ],
+        toDOM(node) {
+          return ['h5', 0];
+        },
+      },
+    });
+    createNode({
+      heading6: {
+        content: 'inline*',
+        group: 'block',
+        defining: true,
+        parseDOM: [
+          {
+            tag: 'h6',
+          },
+        ],
+        toDOM(node) {
+          return ['h6', 0];
         },
       },
     });

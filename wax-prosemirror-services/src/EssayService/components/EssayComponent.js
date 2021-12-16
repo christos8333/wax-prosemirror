a78b13f14fe89e05eda6d90d69ae7aa61179488a
chris <kokosias@yahoo.gr>
2021-12-16 12:52:57 +0200

    add feedback component

--- wax-prosemirror-services/src/EssayService/components/EssayComponent.js
+++ wax-prosemirror-services/src/EssayService/components/EssayComponent.js
@@ -1,7 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import EditorComponent from './EditorComponent';
+import FeedBackComponent from './FeedBackComponent';
 
 export default ({ node, view, getPos }) => {
-  return <EditorComponent getPos={getPos} node={node} view={view} />;
+  return (
+    <>
+      <EditorComponent getPos={getPos} node={node} view={view} />
+      <FeedBackComponent getPos={getPos} node={node} view={view} />
+    </>
+  );
 };

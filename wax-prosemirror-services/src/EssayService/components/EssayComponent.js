d9de45d82f16a92845c6d331283c91a8ee654d1d
chris <kokosias@yahoo.gr>
2021-12-16 12:47:36 +0200

    add essay question node

--- wax-prosemirror-services/src/EssayService/components/EssayComponent.js
+++ wax-prosemirror-services/src/EssayService/components/EssayComponent.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import EditorComponent from './EditorComponent';
 
 export default ({ node, view, getPos }) => {
-  return <span>Essay</span>;
+  return <EditorComponent getPos={getPos} node={node} view={view} />;
 };

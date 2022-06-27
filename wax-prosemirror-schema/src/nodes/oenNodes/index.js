4405fb8c5587fb6f0af3469da012820428d94467
chris <kokosias@yahoo.gr>
2022-06-27 19:38:55 +0300

    add schema

--- wax-prosemirror-schema/src/nodes/oenNodes/index.js
+++ wax-prosemirror-schema/src/nodes/oenNodes/index.js
@@ -1,7 +1,9 @@
+import OenAsideNode from './OenAsideNode';
 import OenContainerNode from './OenContainerNode';
 import OenSectionNode from './OenSectionNode';
 
 export default {
   oen_container: OenContainerNode,
   oen_section: OenSectionNode,
+  oen_aside: OenAsideNode,
 };

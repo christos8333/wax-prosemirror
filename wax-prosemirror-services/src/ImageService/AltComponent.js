1339f5f5ef8a606f6de170e7784762da5aac8c4a
chris <kokosias@yahoo.gr>
2022-09-12 19:24:22 +0300

    add alt overlay in service

---
+++ wax-prosemirror-services/src/ImageService/AltComponent.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import React, { useContext, useLayoutEffect } from 'react';
+import styled from 'styled-components';
+import { WaxContext } from 'wax-prosemirror-core';
+
+export default ({ setPosition, position }) => {
+  const context = useContext(WaxContext);
+  const {
+    activeView,
+    pmViews: { main },
+  } = context;
+
+  const isEditable = main.props.editable(editable => {
+    return editable;
+  });
+
+  const readOnly = !isEditable;
+
+  useLayoutEffect(() => {
+    const WaxSurface = activeView.dom.getBoundingClientRect();
+    const left = 300;
+    const top = 500;
+    setPosition({ ...position, left, top });
+  }, [position.left]);
+
+  if (!readOnly) {
+    return <input type="text" />;
+  }
+};

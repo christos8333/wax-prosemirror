e4bff9d786d26190900dbc92d050085482d23359
chris <kokosias@yahoo.gr>
2022-11-20 12:26:00 +0200

    remove comments

--- wax-prosemirror-core/src/helpers/helpers.js
+++ wax-prosemirror-core/src/helpers/helpers.js
@@ -1,36 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { each } from 'lodash';
 
-// const alterNotesSchema = schema => {
-//   const notes = [];
-//   each(schema.nodes, node => {
-//     if (node.groups.includes('notes')) notes.push(node);
-//   });
-//   if (notes.length > 0) {
-//     notes.forEach(note => {
-//       schema.nodes[note.name].spec.toDOM = node => {
-//         if (node) return [note.name, node.attrs, 0];
-//         return true;
-//       };
-//     });
-//   }
-// };
-
-// const revertNotesSchema = schema => {
-//   const notes = [];
-//   each(schema.nodes, node => {
-//     if (node.groups.includes('notes')) notes.push(node);
-//   });
-//   if (notes.length > 0) {
-//     notes.forEach(note => {
-//       schema.nodes[note.name].spec.toDOM = node => {
-//         if (node) return [note.name, node.attrs];
-//         return true;
-//       };
-//     });
-//   }
-// };
-
 const alterNotesSchema = schema => {
   const notes = [];
   each(schema.nodes, node => {

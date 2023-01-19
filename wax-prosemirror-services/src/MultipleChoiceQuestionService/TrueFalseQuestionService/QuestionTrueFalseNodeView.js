95c6020304b11f83773b18b45ae6879ff3f5d6ca
chris <kokosias@yahoo.gr>
2023-01-19 15:08:00 +0200

    fix mutlitple choice text area

--- wax-prosemirror-services/src/MultipleChoiceQuestionService/TrueFalseQuestionService/QuestionTrueFalseNodeView.js
+++ wax-prosemirror-services/src/MultipleChoiceQuestionService/TrueFalseQuestionService/QuestionTrueFalseNodeView.js
@@ -23,7 +23,7 @@ export default class QuestionTrueFalseNodeView extends QuestionsNodeView {
   }
 
   stopEvent(event) {
-    if (event.target.type === 'text') {
+    if (event.target.type === 'textarea') {
       return true;
     }
     const innerView = this.context.pmViews[this.node.attrs.id];

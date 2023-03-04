ae2cbe1bfe689575ed2bdb18ee7565adb0efe73d
chris <kokosias@yahoo.gr>
2023-03-04 10:10:04 +0200

    core plugins service

---
+++ wax-prosemirror-core/src/config/defaultServices/CorePluginsService/CorePluginsService.js
@@ -0,0 +1,11 @@
+import Service from '../../../Service';
+import FakeCursorPlugin from '../../plugins/FakeCursorPlugin';
+
+export default class CorePluginsService extends Service {
+  boot() {
+    this.app.PmPlugins.add(
+      'fakeCursorPlugin',
+      FakeCursorPlugin('fakeCursorPlugin'),
+    );
+  }
+}

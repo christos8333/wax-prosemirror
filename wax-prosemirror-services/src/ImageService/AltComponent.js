7239db23b2014f49994833c4eb07a6370ba208c0
chris <kokosias@yahoo.gr>
2023-10-17 18:05:30 +0300

    remove focus from alt text

--- wax-prosemirror-services/src/ImageService/AltComponent.js
+++ wax-prosemirror-services/src/ImageService/AltComponent.js
@@ -73,14 +73,14 @@ export default ({ setPosition, position }) => {
 
   const imageConfig = app.config.get('config.ImageService');
   const showAlt = imageConfig && imageConfig.showAlt;
-  const autoFocus =
-    activeView.state.selection &&
-    activeView.state.selection.node &&
-    activeView.state.selection.node.attrs.alt === '';
+  // const autoFocus =
+  //   activeView.state.selection &&
+  //   activeView.state.selection.node &&
+  //   activeView.state.selection.node.attrs.alt === '';
 
   return !readOnly && showAlt ? (
     <StyledInputAlt
-      autoFocus={autoFocus}
+      // autoFocus={autoFocus}
       key="alt"
       onChange={altTextOnChange}
       placeholder="Alt Text"

b7b8f585eb7aeacaf0958cb95186c82830926128
chris <kokosias@yahoo.gr>
2023-12-05 21:01:39 +0200

    heading track changes

---
+++ wax-prosemirror-services/src/TrackChangeService/schema/trackChangesNodes/heading3TrackNode.js
@@ -0,0 +1,41 @@
+import { SchemaHelpers } from 'wax-prosemirror-core';
+
+const heading3 = {
+  content: 'inline*',
+  group: 'block',
+  priority: 0,
+  defining: true,
+  attrs: {
+    id: { default: '' },
+    track: { default: [] },
+    group: { default: '' },
+    viewid: { default: '' },
+  },
+  parseDOM: [
+    {
+      tag: 'h3',
+      getAttrs(hook, next) {
+        Object.assign(hook, {
+          id: hook.dom.dataset.id,
+          track: SchemaHelpers.parseTracks(hook.dom.dataset.track),
+          group: hook.dom.dataset.group,
+          viewid: hook.dom.dataset.viewid,
+        });
+        next();
+      },
+    },
+  ],
+  toDOM(hook, next) {
+    if (hook.node.attrs.track && hook.node.attrs.track.length) {
+      Object.assign(hook.value[1], {
+        'data-id': hook.node.attrs.id,
+        'data-track': JSON.stringify(hook.node.attrs.track),
+        'data-group': hook.node.attrs.group,
+        'data-viewid': hook.node.attrs.viewid,
+      });
+    }
+    next();
+  },
+};
+
+export default heading3;

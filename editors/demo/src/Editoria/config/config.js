b400e256b80a9c49532e7f24a7f98505cef75a35
chris <kokosias@yahoo.gr>
2024-06-30 18:23:52 +0300

    rebase to master

--- editors/demo/src/Editoria/config/config.js
+++ editors/demo/src/Editoria/config/config.js
@@ -229,7 +229,7 @@ export default {
             'StrikeThrough',
           ],
         },
-        // 'BlockQuoteTool',
+        'BlockQuoteTool',
         'HighlightToolGroup',
         'TransformToolGroup',
         'CustomTagInline',

33c3d1aaf8901a79373b075ed81b9bd7affad800
chris <kokosias@yahoo.gr>
2024-07-05 18:28:13 +0300

    add new files

---
+++ wax-prosemirror-services/src/ListsService/BlockQuoteService/schema/blockQuoteNode.js
@@ -0,0 +1,11 @@
+const blockQuoteNode = {
+  content: 'block+',
+  group: 'block',
+  defining: true,
+  parseDOM: [{ tag: 'blockquote' }],
+  toDOM() {
+    return ['blockquote', 0];
+  },
+};
+
+export default blockQuoteNode;

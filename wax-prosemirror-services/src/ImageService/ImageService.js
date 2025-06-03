8f38e44cc9e1b118688b66758243e9a843135786
chris <kokosias@yahoo.gr>
2025-06-03 17:54:36 +0300

    remove figure on typing

--- wax-prosemirror-services/src/ImageService/ImageService.js
+++ wax-prosemirror-services/src/ImageService/ImageService.js
@@ -1,50 +1,55 @@
-import { Service } from 'wax-prosemirror-core'
-import { imageNode, figureCaptionNode, figureNode } from './schema'
-import PlaceHolderPlugin from './plugins/placeHolderPlugin'
-import captionPlugin from './plugins/captionPlugin'
-import selectFigureOnCutPlugin from './plugins/selectFigureOnCutPlugin'
-import Image from './Image'
-import AltComponent from './AltComponent'
-import LongDescComponent from './LongDescComponent'
-import ImageToolGroupService from './ImageToolGroupService/ImageToolGroupService'
-import './image.css'
+import { Service } from 'wax-prosemirror-core';
+import { imageNode, figureCaptionNode, figureNode } from './schema';
+import PlaceHolderPlugin from './plugins/placeHolderPlugin';
+import captionPlugin from './plugins/captionPlugin';
+import selectFigureOnCutPlugin from './plugins/selectFigureOnCutPlugin';
+import removeFigureOnTyping from './plugins/removeFigureOnTyping';
+import Image from './Image';
+import AltComponent from './AltComponent';
+import LongDescComponent from './LongDescComponent';
+import ImageToolGroupService from './ImageToolGroupService/ImageToolGroupService';
+import './image.css';
 
 class ImageService extends Service {
-  name = 'ImageService'
+  name = 'ImageService';
 
   boot() {
     this.app.PmPlugins.add(
       'selectFigureOnCutPlugin',
       selectFigureOnCutPlugin('selectFigureOnCutPlugin'),
-    )
+    );
+    this.app.PmPlugins.add(
+      'removeFigureOnTyping',
+      removeFigureOnTyping('removeFigureOnTyping'),
+    );
     this.app.PmPlugins.add(
       'imagePlaceHolder',
       PlaceHolderPlugin('imagePlaceHolder'),
-    )
-    this.app.PmPlugins.add('caption', captionPlugin('caption'))
+    );
+    this.app.PmPlugins.add('caption', captionPlugin('caption'));
   }
 
   register() {
-    this.container.bind('Image').to(Image)
-    const createNode = this.container.get('CreateNode')
-    const createOverlay = this.container.get('CreateOverlay')
+    this.container.bind('Image').to(Image);
+    const createNode = this.container.get('CreateNode');
+    const createOverlay = this.container.get('CreateOverlay');
     createNode({
       figure: figureNode,
-    })
+    });
 
     createNode(
       {
         image: imageNode,
       },
       { toWaxSchema: true },
-    )
+    );
     createNode(
       {
         figcaption: figureCaptionNode,
       },
       // ,
       // { toWaxSchema: true },
-    )
+    );
 
     createOverlay(
       AltComponent,
@@ -56,7 +61,7 @@ class ImageService extends Service {
         followCursor: false,
         selection: false,
       },
-    )
+    );
 
     createOverlay(
       LongDescComponent,
@@ -68,10 +73,10 @@ class ImageService extends Service {
         followCursor: false,
         selection: false,
       },
-    )
+    );
   }
 
-  dependencies = [new ImageToolGroupService()]
+  dependencies = [new ImageToolGroupService()];
 }
 
-export default ImageService
+export default ImageService;

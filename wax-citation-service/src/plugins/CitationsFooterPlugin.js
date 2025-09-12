c47cf7848c8f8d4876f44bf3183ddd51777849cb
chris <kokosias@yahoo.gr>
2025-09-12 15:22:27 +0300

    typing under citations

--- wax-citation-service/src/plugins/CitationsFooterPlugin.js
+++ wax-citation-service/src/plugins/CitationsFooterPlugin.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-plusplus */
+
 /* eslint-disable camelcase */
+
 import { Plugin, PluginKey } from 'prosemirror-state';
 
 const citationsFooterPlugin = new PluginKey('citationsFooterPlugin');
@@ -7,9 +10,36 @@ export default (key, app) => {
   return new Plugin({
     key: citationsFooterPlugin,
     appendTransaction(transactions, oldState, newState) {
-      const { tr } = newState;
+      const { doc, tr } = newState;
       const lastNode = newState.doc.lastChild;
       const { citations_data_node } = newState.schema.nodes;
+
+      // A more reliable way to check if there is more than one footer
+      let existingFooter = {};
+      let footerCount = 0;
+      doc.descendants((node, pos) => {
+        if (node.type.name === 'citations_data_node') {
+          footerCount++;
+          existingFooter = {
+            node,
+            pos,
+          };
+        }
+      });
+
+      if (footerCount > 1) {
+        // If there are multiple, delete all but the last one
+        const deleteTr = newState.tr;
+        doc.descendants((node, pos) => {
+          if (
+            node.type.name === 'citations_data_node' &&
+            node !== doc.lastChild
+          ) {
+            deleteTr.delete(pos, pos + node.nodeSize);
+          }
+        });
+        return deleteTr;
+      }
 
       if (!lastNode || lastNode.type !== citations_data_node) {
         const newFooter = citations_data_node.create({
@@ -21,6 +51,7 @@ export default (key, app) => {
       if (tr.docChanged) {
         return tr;
       }
+      return tr;
     },
   });
 };
